85d08ed4745469b4d0672889798c98d29728eda9
Emi Roberti <114434826+EmiRoberti77@users.noreply.github.com>
2024-12-30 07:44:04 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,10 @@ const emi: User = {
   firstName: "Emi",
   lastName: "Roberti",
   email: "emi@emi.ai",
-  mobile: "+447500994545",
+  mobile: "+447501774545",
   age: 47,
   freinds: ["friend 1", "friend 2"],
-  dob: new Date(1977, 5, 25),
+  dob: new Date(1977, 4, 12),
   details: {
     isSubscribed: true,
   },
